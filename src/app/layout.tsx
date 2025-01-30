ad0eff173519ddf411dc04247c4b2c6300cdd9b3
Dylan Vidal <dylanvidal1204@gmail.com>
2025-01-29 19:09:28 -0500

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "~/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
+import Script from "next/script";
 import Footer from "~/components/footer";
 import SpecialsDialog from "~/components/specials-dialog";
 import { Toaster } from "~/components/ui/toaster";
@@ -30,6 +31,11 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable} bg-lime-100`}>
+      <Script
+        async
+        src="https://dormdevs-analytics.vercel.app/script.js"
+        data-website-id="a429b59d-4627-4f31-9599-47af68fbab85"
+      />
       <body>
         <main className="h-screen overflow-x-hidden bg-lime-100">
           <SpecialsDialog />
